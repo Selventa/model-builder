0266a7d9b1b3743fc8540c85c1d76818530af2e0
Anthony <abargnesi@selventa.com>
2013-09-25 15:53:04 -0400

    completed conversion, CyNetworkView <-> Map
    refs #230

--- modules/core/src/model/builder/core/ModelUtil.groovy
+++ modules/core/src/model/builder/core/ModelUtil.groovy
@@ -8,6 +8,7 @@ import wslite.json.JSONArray
 import static model.builder.core.Util.addMetadata
 import static model.builder.core.Util.createColumn
 import static org.cytoscape.model.CyNetwork.*
+import static org.cytoscape.model.CyEdge.INTERACTION
 import static org.cytoscape.view.presentation.property.BasicVisualLexicon.NODE_X_LOCATION
 import static org.cytoscape.view.presentation.property.BasicVisualLexicon.NODE_Y_LOCATION
 
@@ -21,26 +22,26 @@ class ModelUtil {
         createColumn(locals, 'who', String.class, true, null)
         createColumn(locals, 'when', String.class, true, null)
         createColumn(locals, 'comment', String.class, true, null)
-        cyN.getRow(cyN, LOCAL_ATTRS).set(NAME, "${network.name} (Revision $number)" as String)
+        createColumn(locals, 'revision_number', Integer.class, true, null)
+        cyN.getRow(cyN, LOCAL_ATTRS).set(NAME, network.name)
         cyN.getRow(cyN, LOCAL_ATTRS).set('who', revision.who)
         cyN.getRow(cyN, LOCAL_ATTRS).set('when', revision.when)
         cyN.getRow(cyN, LOCAL_ATTRS).set('comment', revision.comment)
+        cyN.getRow(cyN, LOCAL_ATTRS).set('revision_number', number)
         addMetadata(network.metadata as Map, cyN, locals)
 
-        def Map<Integer, CyNode> nodes = [:]
+        def Map<Integer, CyNode> index = [:]
         network.nodes.collect { JSONArray node ->
             def (Integer id, label) = node
-            CyNode cyNode = nodes[id] ?: (nodes[id] = cyN.addNode())
+            CyNode cyNode = index[id] ?: (index[id] = cyN.addNode())
             cyN.getRow(cyNode).set(NAME, label)
-            if (node[4]) {
-                addMetadata(node[4].metadata as Map, cyN, locals)
-            }
+            if (node[4]) addMetadata(node[4].metadata as Map, cyN, locals)
             cyNode
         }
         network.edges.collect { JSONArray edge ->
             def (src, tgt, rel, meta, evidence) = edge
-            CyNode cySource = nodes[src] ?: (nodes[src] = cyN.addNode())
-            CyNode cyTarget = nodes[tgt] ?: (nodes[tgt] = cyN.addNode())
+            CyNode cySource = index[src] ?: (index[src] = cyN.addNode())
+            CyNode cyTarget = index[tgt] ?: (index[tgt] = cyN.addNode())
             def cyEdge = cyN.addEdge(cySource, cyTarget, true)
             cyN.getRow(cyEdge).set('interaction', rel)
             if (meta) {
@@ -52,10 +53,46 @@ class ModelUtil {
         def cyNv = cyRef.cyNetworkViewFactory.createNetworkView(cyN)
         network.nodes.each { JSONArray node ->
             def (Integer id, _, x, y) = node
-            CyNode cyNode = nodes[id]
+            CyNode cyNode = index[id]
             cyNv.getNodeView(cyNode).setVisualProperty(NODE_X_LOCATION, x as Double)
             cyNv.getNodeView(cyNode).setVisualProperty(NODE_Y_LOCATION, y as Double)
         }
         cyNv
+    }
+
+    static Map from(CyNetworkView cyNv) {
+        def cyN = cyNv.model
+        def cyNr = cyN.getRow(cyN)
+
+        // TODO convert metadata
+
+        // convert nodes
+        def counter = 0
+        def Map<CyNode, Integer> index = [:]
+        def nodes = cyN.nodeList.collect { cyNode ->
+            def name = cyN.getRow(cyNode).get(NAME, String.class)
+            Double x = cyNv.getNodeView(cyNode).getVisualProperty(NODE_X_LOCATION)
+            Double y = cyNv.getNodeView(cyNode).getVisualProperty(NODE_Y_LOCATION)
+            index[cyNode] = counter
+            [counter++, name, x as Integer, y as Integer, null]
+        }
+
+        // convert edges
+        def edges = cyN.edgeList.collect { cyEdge ->
+            def rel = cyN.getRow(cyEdge).get(INTERACTION, String.class)
+            [index[cyEdge.source], index[cyEdge.target], rel, null, null]
+        }
+
+        [
+            network: [
+                name: cyNr.get(NAME, String.class),
+                description: cyNr.get('description', String.class, null),
+                species: cyNr.get('species', String.class, null),
+                reference_node: cyNr.get('reference_node', String.class, null),
+                metadata: null,
+                nodes: nodes,
+                edges: edges
+            ]
+        ]
     }
 }
