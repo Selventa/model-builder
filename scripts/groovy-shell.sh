2027587a2a2359034d047c83906dc50ac1db4eee
Anthony <abargnesi@selventa.com>
2013-10-23 08:57:05 -0400

    groovy shell now loads libs/ as classpath in env

--- scripts/groovy-shell.sh
+++ scripts/groovy-shell.sh
@@ -3,4 +3,9 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../
 cd "${DIR}" || exit 1
 . env.sh || exit 1
 
-exec "${DEV_TOOLS_GROOVY_DIR}/bin/groovysh"
+CLASSPATH="."
+for lib in $(find . -path "./libs*" -name "*.jar" -not -name "groovy-all*.jar"); do
+    CLASSPATH="$CLASSPATH:$lib"
+done
+
+exec "${DEV_TOOLS_GROOVY_DIR}/bin/groovysh" -classpath "$CLASSPATH"
